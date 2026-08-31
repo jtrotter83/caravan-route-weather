1a1917bae0eab0d50888d4c90db980d395812fce
jtrotter83 <jas.trotter@gmail.com>
2026-08-31 11:43:03 +0000

    feat: add fetch diagnostics page at #/debug for mobile debugging

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { DebugFetchPage } from './components/DebugFetchPage';
 import './styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {window.location.hash === '#/debug' ? <DebugFetchPage /> : <App />}
   </StrictMode>,
 );
